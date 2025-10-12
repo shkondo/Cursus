54bdaadbb671cd29dd3e3bdb420f42044f2fbc4d
djspright <gdlnolgs@gmail.com>
2025-10-12 12:52:41 +0900

    deleted unused file and function

--- push_swap/src/stack/list_operations.c
+++ push_swap/src/stack/list_operations.c
@@ -28,53 +28,6 @@ void	push_front(t_stack *stack, int value)
 	stack->size++;
 }
 
-void	push_back(t_stack *stack, int value)
-{
-	t_node	*new_node;
-
-	if (!stack || !stack->head)
-		return ;
-	new_node = create_node(value);
-	if (!new_node)
-		return ;
-	new_node->prev = stack->head->prev;
-	new_node->next = stack->head;
-	stack->head->prev->next = new_node;
-	stack->head->prev = new_node;
-	stack->size++;
-}
-
-int	pop_front(t_stack *stack)
-{
-	t_node	*to_remove;
-	int		value;
-
-	if (!stack || !stack->head || stack->size == 0)
-		return (0);
-	to_remove = stack->head->next;
-	value = to_remove->value;
-	stack->head->next = to_remove->next;
-	to_remove->next->prev = stack->head;
-	free(to_remove);
-	stack->size--;
-	return (value);
-}
-
-int	pop_back(t_stack *stack)
-{
-	t_node	*to_remove;
-	int		value;
-
-	if (!stack || !stack->head || stack->size == 0)
-		return (0);
-	to_remove = stack->head->prev;
-	value = to_remove->value;
-	stack->head->prev = to_remove->prev;
-	to_remove->prev->next = stack->head;
-	free(to_remove);
-	stack->size--;
-	return (value);
-}
 
 int	get_at_index(t_stack *stack, int index)
 {
