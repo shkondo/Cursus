56f1a666f43801664614a00e32a85bf700e6cebc
djspright <gdlnolgs@gmail.com>
2026-03-15 00:44:19 +0900

    feat: done cpp_00/ex00

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -6,7 +6,25 @@
 /*   By: shkondo <shkondo@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 22:59:22 by shkondo           #+#    #+#             */
-/*   Updated: 2026/03/14 22:59:23 by shkondo          ###   ########.fr       */
+/*   Updated: 2026/03/15 00:38:34 by shkondo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+	if(argc <= 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 1;
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j]; j++)
+			std::cout << static_cast<char>(std::toupper(argv[i][j]));
+	}
+	std::cout << std::endl;
+	return 0;
+}
